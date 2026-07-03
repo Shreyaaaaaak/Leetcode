7fbcaecf3d05c570a9a37702935ad3191f5c1998
Shreya Kaushik <wntersolier2308@gmail.com>
2026-07-03 22:18:58 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.61%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def tribonacci(self, n):
+        memo = {}
+
+        def solve(n):
+            if n == 0:
+                return 0
+            if n <= 2:
+                return 1
+
+            if n in memo:
+                return memo[n]
+
+            memo[n] = solve(n-1) + solve(n-2) + solve(n-3)
+            return memo[n]
+
+        return solve(n)
